c11fdeecb4cc932ff0731121d0ad3b63e0c4b2c0
Frederik Schönfeldt <frederikschoenfeldt@gmail.com>
2026-06-28 20:54:16 +0200

    fix: bring back mix check to dev

--- mix.exs
+++ mix.exs
@@ -113,7 +113,7 @@ defmodule OwnershipAshChat.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
       {:bandit, "~> 1.5"},
-      {:ex_check_ng, "1.0.0-rc.0", only: [:test], runtime: false}
+      {:ex_check_ng, "1.0.0-rc.0", only: [:dev, :test], runtime: false}
     ]
   end
 
